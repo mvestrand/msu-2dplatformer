c715775646c45c1061c407056de516f76cf130bb
mvestrand <10858947+mvestrand@users.noreply.github.com>
2026-06-12 13:00:05 -0600

    Level 1 End Made

---
+++ base/scripts/Goal.cs
@@ -0,0 +1,23 @@
+using Godot;
+using System;
+
+public class Goal : Area2D {
+    // Declare member variables here. Examples:
+    // private int a = 2;
+    // private string b = "text";
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready() {
+        
+    }
+
+    
+	public void _on_area_entered(Area2D other) {
+		// Victory
+        
+	}
+
+	public void _on_body_entered(Node other) {
+		// Victory
+	}
+}
