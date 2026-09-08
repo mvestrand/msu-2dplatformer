3b69ad5dabd4bba640b03c7ac9acecb07fc30049
mvestrand <10858947+mvestrand@users.noreply.github.com>
2026-09-08 12:00:06 -0600

    End of level room

--- base/scripts/Goal.cs
+++ base/scripts/Goal.cs
@@ -1,23 +1,47 @@
 using Godot;
 using System;
 
-public class Goal : Area2D {
+public partial class Goal : Area2D {
     // Declare member variables here. Examples:
     // private int a = 2;
     // private string b = "text";
 
+    [Export] public PackedScene nextLevel;
+    [Export] public PackedScene winScreen;
+
+    private bool hasWon = false;
+
     // Called when the node enters the scene tree for the first time.
     public override void _Ready() {
-        
+        hasWon = false;
+        Connect("area_entered", this, nameof(_on_area_entered));
+		Connect("body_entered", this, nameof(_on_body_entered));
+
     }
+
+
+    public void ShowWinScreen() {
+		hasWon = true;
+		GetTree().Paused = true;
+		LevelClear inst = (LevelClear)winScreen.Instance();
+		inst.nextLevel = nextLevel;
+		GetTree().Root.AddChild(inst);
+	}
 
     
 	public void _on_area_entered(Area2D other) {
 		// Victory
-        
+        if (!hasWon) {
+			ShowWinScreen();
+            hasWon = true;
+        }
 	}
 
 	public void _on_body_entered(Node other) {
 		// Victory
+		if (!hasWon) {
+            ShowWinScreen();
+            hasWon = true;
+        }
 	}
 }
